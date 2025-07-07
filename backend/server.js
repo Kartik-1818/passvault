51318797f7556ca151733004684f9246b21f64ae
Kartik Jhamb <241230026@nitdelhi.ac.inm>
2025-07-07 21:37:26 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,9 @@ app.use((req, res, next) => {
 // CORS Setup
 app.use(
   cors({
-    origin: "https://vault-blond.vercel.app", // Replace with your frontend URL
+    origin:[ "https://vault-blond.vercel.app", // Replace with your frontend URL
+     "https://vault-blond.vercel.app", // ✅ your current frontend
+  "https://passvault-pi.vercel.app"],// optional if still used
     methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
     credentials: true, // If you need to send cookies
   })
